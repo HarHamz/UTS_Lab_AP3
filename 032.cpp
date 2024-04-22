d43feef1adafb095b2bfe741688003a3e674d701
parulls <paruliandwireslia22@gmail.com>
2024-04-22 21:05:46 +0700

    Perbaikan tipe kartu (2)

--- 032.cpp
+++ 032.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 main (){
     string nomorKartu;
-    int digitKartu,checksum;
     string tipeKartu;
     cout<<"Masukkan nomor kartu : ";
     cin>>nomorKartu;
-        if((nomorKartu.length()==16)&&((nomorKartu[0]==51||nomorKartu[0]==52||nomorKartu[0]==53||nomorKartu[0]==54||nomorKartu[0]==55)))
+        if(nomorKartu.length()==16)
         {
-            tipeKartu="MASTERCARD";
+            if ((nomorKartu[0] == '5')&&(nomorKartu[1]=='1'||nomorKartu[1]=='2'||nomorKartu[1]=='3'||nomorKartu[1]=='4'||nomorKartu[1]=='5')) 
+            tipeKartu = "MASTERCARD";
+           else if (nomorKartu[0] == '4') 
+            tipeKartu = "VISA";
+            else tipeKartu="Tidak Valid";
         }
-        else if (((nomorKartu.length()==13)&&(nomorKartu[0]==4))||((nomorKartu.length()==13)&&(nomorKartu[0]==4))){
+        else if ((nomorKartu.length()==13)&&(nomorKartu[0]=='4')){
             tipeKartu="VISA";
         }
-    cout<<digitKartu<<endl;
+        else tipeKartu="Tidak Valid";
     cout<<"Nomor kartu anda : "<<nomorKartu<<endl;
     cout<<"Tipe kartu anda : "<<tipeKartu<<endl;
-    cout<<"Checksum : "<<checksum<<endl;
+    return 0;
 }
