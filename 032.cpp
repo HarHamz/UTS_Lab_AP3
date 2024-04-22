d48d952b98e9c8ab3388c9646d29d8c9e424822e
parulls <paruliandwireslia22@gmail.com>
2024-04-23 01:03:40 +0700

    meminta inputan kartu

---
+++ 032.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main (){
+    long long nomorKartu;
+    do 
+    {
+    cout<<"Masukkan nomor kartu : ";
+    cin>>nomorKartu;
+    }
+    while(nomorKartu<=0);
+}
