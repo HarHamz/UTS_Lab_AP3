f60367f76c01611f68453b4a08d620a82dd691bb
parulls <paruliandwireslia22@gmail.com>
2024-04-23 01:19:52 +0700

    mencari digit akhir dari tiap 2 digit nomor  kartu

--- 032.cpp
+++ 032.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 main (){
     long long nomorKartu;
+    int digitAkhir,jumlah;
     do 
     {
     cout<<"Masukkan nomor kartu : ";
     cin>>nomorKartu;
     }
     while(nomorKartu<=0);
+
+    while(nomorKartu>0){
+        digitAkhir=nomorKartu%10;
+        nomorKartu/=100;
+        cout<<digitAkhir<<" ";
+    }
+    
 }
