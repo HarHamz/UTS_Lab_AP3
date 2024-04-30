dfe96793d61839a80f88302a273b5251fb8a4d72
rielyta <desi@students.usu.ac.id>
2024-04-30 21:52:48 +0700

    fungsi 2

--- 044.cpp
+++ 044.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-//fungsi untuk mwnghitung checksum
+//fungsi untuk menghitung checksum
 int hitungchecksum(long long angka_kartu){
     int jumlah=0,digit,i=1;
+    /*
+    jumlah diinisialisasi dengan 0, digunakan untuk menghitung jumlah akhir
+    digit utk menyimpan digit terakhir dari nomor kartu
+    i diinisialisasi dengan 1, digunakan untuk menentukan digit genap atau ganjil
+    */
     while(angka_kartu>0) {
         digit = angka_kartu%10;
+        //mengambil digit terakhir dari angka_kartu
         if (i%2==0) {
+            /*
+            jika i genap, digit dikali dua kemudian jika hasilnya lebih dari 9,
+            maka digit tersebut akan diubah menjadi jumlah dari dua digit tersebut
+            */
             digit = digit*2;
             if (digit>9) {
                 digit = digit/10+digit%10;
             }
         }
         jumlah = jumlah+digit;
+        //digit ditambahkan ke jumlah
         angka_kartu = angka_kartu/10;
+        //angka_kartu dibagi 10 untuk menghapus digit terakhir
         i++;
+        //i ditambahkan 1
     }
     return jumlah;
+    //mengembalikan jumlah
 }
 
 //fungsi untuk menentukan tipe kartu
@@ -27,17 +42,21 @@ string tipekartu(long long nomor_kartu, int checksum) {
             tipe_kartu = "Mastercard";
         } else {
             tipe_kartu = "Tidak Valid";
+            //jika checksum tidak habis dibagi 10, maka kartu tidak valid
         }
     } else if ((nomor_kartu / 1000000000000000 == 4) || (nomor_kartu / 1000000000000 == 4)) {
         if (checksum % 10 == 0) {
             tipe_kartu = "Visa";
         } else {
             tipe_kartu = "Tidak Valid";
+            //jika checksum tidak habis dibagi 10, maka kartu tidak valid
         }
     } else {
         tipe_kartu = "tidak diketahui";
+        //jika nomor kartu tidak sesuai dengan tipe kartu yang ada
     }
     return tipe_kartu;
+    //mengembalikan tipe kartu
 }
 
 int main() {
